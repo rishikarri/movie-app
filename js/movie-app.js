5dd17a4df3bdb129336ef6f89cbb582fd51b2a5c
Rishi Karri <rishikarri1@gmail.com>
2016-12-21 10:07:39 -0500

    Initial Commit

---
+++ js/movie-app.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function(){
+	console.log("hey");
+
+	// when it comes time to add movies, we are going to apprend movies to this url
+	var apiBaseURL = 'http://api.themoviedb.org/3/'
+
+	var imageBaseUrl = 'http://image.tmdb.org/t/p/';
+
+	const nowPlayingUrl = apiBaseURL + "movie/now_playing" + 
+})
